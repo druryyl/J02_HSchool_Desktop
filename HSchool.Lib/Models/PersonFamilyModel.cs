559531ce46855a39aa70392e7a85603a8691077b
Jude <drury_yl@yahoo.com>
2021-08-13 19:12:00 +0700

    -update package (dapper, nunalib)

---
+++ HSchool.Lib/Models/PersonFamilyModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSchool.Lib.Models
+{
+    public class PersonFamilyModel : IPersonKey
+    {
+        public string PersonID { get; set; }
+    }
+}
